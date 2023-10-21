4f555e7b86e22b937285ec57ad5136df0173069b
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2023-10-21 15:16:36 -0300

    💚 Updated interface tests

--- source/interface.cpp
+++ source/interface.cpp
@@ -1,16 +1,58 @@
 #include "../interface/interface.hpp"
+#include <memory>
+
+int MainMenu() {
+   std::unique_ptr<ext::vmenu> main_menu{
+       std::make_unique<ext::vmenu>("Main menu!")};
+   main_menu->insert_option(1, "Play");
+   main_menu->insert_option(2, "About");
+   main_menu->insert_option(-1, "Exit");
+
+   while (!main_menu->exit()) {
+      main_menu->render();
+      main_menu->process();
+   }
+
+   return main_menu->get_selected();
+}
+
+int PlayMenu() {
+   std::unique_ptr<ext::hmenu> play_menu{
+       std::make_unique<ext::hmenu>("Play menu!")};
+   play_menu->insert_option(2, "About");
+   play_menu->insert_option(0, "Return");
+
+   while (!play_menu->exit()) {
+      play_menu->render();
+      play_menu->process();
+   }
+
+   return play_menu->get_selected();
+}
+
+int AboutMenu() {
+   std::unique_ptr<ext::hmenu> about_menu{
+       std::make_unique<ext::hmenu>("About menu!")};
+   about_menu->insert_option(0, "Return");
+
+   while (!about_menu->exit()) {
+      about_menu->render();
+      about_menu->process();
+   }
+
+   return about_menu->get_selected();
+}
 
 int main() {
-   ext::vmenu vertical_menu;
-   vertical_menu.set_title("\t\tMeu titulo!\n");
-   vertical_menu.insert_option(100, "Play");
-   vertical_menu.insert_option(200, "Load");
-   vertical_menu.insert_option(300, "About");
-   vertical_menu.insert_option(400, "Exit");
-
-   while(!vertical_menu.exit()) {
-      vertical_menu.render();
-      vertical_menu.process();
+   int choice = 0;
+   while (choice != -1) {
+      if (choice == 0) {
+         choice = MainMenu();
+      } else if (choice == 1) {
+         choice = PlayMenu();
+      } else {
+         choice = AboutMenu();
+      }
    }
 
    return 0;
